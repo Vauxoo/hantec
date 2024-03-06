755eba8ed227d9b97c6f4644db116eb2689b8fda
DESKTOP-IQ6LM3K\Jose Antonio <jsalas@dotkomma.mx>
2024-03-06 10:18:28 -0600

    fix: depends en manifest

--- hantec_api_ecommerce/__manifest__.py
+++ hantec_api_ecommerce/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "1.0.0",
     "author": "C&O PROJECTS AND SOLUTIONS",
     "category": "Sales Managment",
-    "depends": ["sales_management"],
+    "depends": ["sale_management"],
     "installable": True,
     "development_status": "Production/Stable",
     "maintainers": ["C&O PROJECTS AND SOLUTIONS"],
